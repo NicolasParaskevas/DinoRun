ec64ebe09e350ad8f671be0d4259af86139a255f
NicolasParaskevas <nickrock126@gmail.com>
2020-04-17 01:27:24 +0300

    Added Cloud class

---
+++ Objects/Cloud.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinoRun.Objects
+{
+    public class Cloud
+    {
+        private Vector2 Position;
+        private Texture2D Sprite;
+        private float Speed;
+        
+        public Cloud(Vector2 position, Texture2D sprite, float speed)
+        {
+            Position = position;
+            Speed = speed;
+            Sprite = sprite;
+        }
+
+        public void Update(GameTime gameTime) 
+        {
+            Position.X -= Speed;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(Sprite, Position, Color.White);
+        }
+    }
+}
